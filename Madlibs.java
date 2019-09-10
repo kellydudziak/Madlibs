946f13e174ffd75fbcc12a37e405ab30628dd84f
kellydudziak <kellydudziak@gmail.com>
2019-09-10 12:15:04 -0700

    Fixed compile errors.

--- Madlibs.java
+++ Madlibs.java
@@ -1,32 +1,32 @@
-package edu.mills.CS63;
-
 import java.util.Scanner;
 
 public class Madlibs {
 	
-	private String personsName;
-	private String adj1;
-	private int num1;
-	private int num2;
-	private String place;
-	private String adj2;
-	private String noun;
-	private String food;
-	private String songTitle;
-	private String verb;
-	private String adj3;
+	private static String personsName;
+	private static String adj1;
+	private static int num1;
+	private static int num2;
+	private static String place;
+	private static String adj2;
+	private static String noun;
+	private static String food;
+	private static String songTitle;
+	private static String verb;
+	private static String adj3;
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		getInput();
 		printStory();
 	}
 	
-	private void getInput() {
+	private static void getInput() {
 		Scanner keyboard = new Scanner(System.in);
 		
 		// can you rearrange so it's all prompts and then all parsing?
 		System.out.print("Enter a person's name: ");
 		personsName = keyboard.nextLine();
+    System.out.print("Enter an adjective: ");
+		adj2 = keyboard.nextLine();
 		System.out.print("Enter a number: ");
 		num1 = Integer.parseInt(keyboard.nextLine());
 		System.out.print("Enter a number: ");
@@ -34,30 +34,32 @@ public class Madlibs {
 		System.out.print("Enter a place: ");
 		place =keyboard.nextLine();
 		System.out.print("Enter an adjective: ");
-		adj2 =keyboard.nextLine();
+		adj2 = keyboard.nextLine();
+    		System.out.print("Enter a noun: ");
+    		noun = keyboard.nextLine();
 		System.out.print("Enter a food: ");
-		food =keyboard.nextLine();
+		food = keyboard.nextLine();
 		System.out.print("Enter the title of a song: ");
-		songTitle =keyboard.nextLine();
+		songTitle = keyboard.nextLine();
 		System.out.print("Enter a verb: ");
-		verb =keyboard.nextLine();
+		verb = keyboard.nextLine();
 		System.out.print("Enter an adjective: ");
-		adj3 =keyboard.nextLine();
+		adj3 = keyboard.nextLine();
 		
 		keyboard.close();
 	}
 	
-	private void printStory() {
-		System.out.println(personsName + "was feeling very " +
+	private static void printStory() {
+		System.out.println(personsName + " was feeling very " +
 				adj1 + " today, because today they turned " +
 				num1 + " years old! In just a few minutes, " +
 				num2 + " of their friends would be coming to their " +
 				place + " for their birthday party. The " +
 				place + " was covered in " +
 				adj2 + " " + noun + " decorations and their birthday " +
-				food + " was waiting on the talbe. As soon as their friends came, they would sing '" +
+				food + " was waiting on the table. As soon as their friends came, they would sing '" +
 				songTitle + "' to them and everyone would " +
-				verb + " the " + food + "! it was going to be a " +
+				verb + " the " + food + "! It was going to be a " +
 				adj3 + " day.");
 			
 	}
